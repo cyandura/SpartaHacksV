ad30d51deaf167d68f0fdfde77cefecb403c5096
Cody Yandura <35173272+cyandura@users.noreply.github.com>
2019-01-20 09:04:54 -0500

    got rid of docx

--- LinguaGUI.py
+++ LinguaGUI.py
@@ -65,8 +65,8 @@ filename1.grid(row=1, column=1)
 gridframe.grid(row=0,column=1)
 
 
-convert.grid(row=3)
-saveas.grid(row=3,column=1)
+convert.grid(row=3,column=1)
+
 
 def recordmenu():
     recordframe.pack(side = LEFT)
@@ -105,12 +105,11 @@ R1 = Radiobutton(m, text='Record', variable=v, value=1, command = lambda : recor
 R2 = Radiobutton(m, text='Convert', variable=v, value=2,command = lambda : saveandconvertmenu()).pack(anchor=W)
 
 R3 = Radiobutton(saveandconvertframe, text='.PDF', variable=v, value=3, command = lambda : pdf()).grid(row=1,column=0)
-R4 = Radiobutton(saveandconvertframe, text='.DOCX', variable=v, value=4,command = lambda : docx()).grid(row=1,column=1)
-R5 = Radiobutton(saveandconvertframe, text='.TXT', variable=v, value=5,command = lambda : txt()).grid(row=1,column=2)
+R5 = Radiobutton(saveandconvertframe, text='.TXT', variable=v, value=5,command = lambda : txt()).grid(row=1,column=1)
 
 R6 = Radiobutton(recordframe, text='.PDF', variable=v, value=6, command = lambda : pdf()).grid(row=2,column=0)
-R7 = Radiobutton(recordframe, text='.DOCX', variable=v, value=7,command = lambda : docx()).grid(row=2,column=1)
-R8 = Radiobutton(recordframe, text='.TXT', variable=v, value=8,command = lambda : txt()).grid(row=2,column=2)
+
+R8 = Radiobutton(recordframe, text='.TXT', variable=v, value=8,command = lambda : txt()).grid(row=2,column=1)
 
 
 m.mainloop()
